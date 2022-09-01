3be06bf47f4a2618b1fe7fb127a3babbe7c71e6a
f0reachARR <f0reach@f0reach.me>
2022-09-01 02:22:17 +0000

    feat(ros/topic): add topic subscriber

---
+++ src/ros/cppmsg_constructor.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+
+class MessageConstructor
+{
+private:
+    static constexpr auto INTROSPECTION_IDENTIFIER = "rosidl_typesupport_introspection_cpp";
+
+public:
+    explicit MessageConstructor(const std::string data_type)
+    {
+        using rosidl_typesupport_introspection_cpp::MessageMembers;
+
+        auto intro_lib = rclcpp::get_typesupport_library(data_type, INTROSPECTION_IDENTIFIER);
+        auto typesupport = rclcpp::get_typesupport_handle(data_type, INTROSPECTION_IDENTIFIER, *intro_lib);
+
+        members_ = static_cast<const MessageMembers *>(typesupport->data);
+    }
+
+    std::shared_ptr<void> create_message()
+    {
+        using rosidl_runtime_cpp::MessageInitialization;
+
+        auto buffer = calloc(1, members_->size_of_);
+        members_->init_function(buffer, MessageInitialization::ZERO);
+
+        auto deleter = [&](void *buffer)
+        {
+            members_->fini_function(buffer);
+            free(buffer);
+        };
+        return std::shared_ptr<void>(buffer, deleter);
+    }
+
+private:
+    const rosidl_typesupport_introspection_cpp::MessageMembers *members_;
+};
