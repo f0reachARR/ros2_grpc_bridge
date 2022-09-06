9ac372de344e43c96da8852fa0a5188fba03913c
f0reachARR <f0reach@f0reach.me>
2022-09-06 08:36:52 +0000

    gRPC wip

---
+++ src/grpc/ros_reflection.hpp
@@ -0,0 +1,208 @@
+#pragma once
+
+#include <grpcpp/grpcpp.h>
+
+#include "reflection.pb.h"
+#include "reflection.grpc.pb.h"
+
+using namespace grpc;
+
+class ProtoServerReflection final
+    : public grpc::reflection::v1alpha::ServerReflection::Service
+{
+public:
+    ProtoServerReflection() : descriptor_pool_(protobuf::DescriptorPool::generated_pool()) {}
+
+    // Add the full names of registered services
+    void SetServiceList(const std::vector<std::string> *services);
+
+    // implementation of ServerReflectionInfo(stream ServerReflectionRequest) rpc
+    // in ServerReflection service
+    grpc::Status ServerReflectionInfo(
+        grpc::ServerContext *context,
+        grpc::ServerReaderWriter<grpc::reflection::v1alpha::ServerReflectionResponse,
+                                 grpc::reflection::v1alpha::ServerReflectionRequest> *stream)
+        override
+    {
+        using grpc::reflection::v1alpha::ServerReflectionRequest;
+        using grpc::reflection::v1alpha::ServerReflectionResponse;
+        ServerReflectionRequest request;
+        ServerReflectionResponse response;
+        Status status;
+        while (stream->Read(&request))
+        {
+            switch (request.message_request_case())
+            {
+            case ServerReflectionRequest::MessageRequestCase::kFileByFilename:
+                status = GetFileByName(context, request.file_by_filename(), &response);
+                break;
+            case ServerReflectionRequest::MessageRequestCase::kFileContainingSymbol:
+                status = GetFileContainingSymbol(
+                    context, request.file_containing_symbol(), &response);
+                break;
+            case ServerReflectionRequest::MessageRequestCase::
+                kFileContainingExtension:
+                status = GetFileContainingExtension(
+                    context, &request.file_containing_extension(), &response);
+                break;
+            case ServerReflectionRequest::MessageRequestCase::
+                kAllExtensionNumbersOfType:
+                status = GetAllExtensionNumbers(
+                    context, request.all_extension_numbers_of_type(),
+                    response.mutable_all_extension_numbers_response());
+                break;
+            case ServerReflectionRequest::MessageRequestCase::kListServices:
+                // status =
+                //     ListService(context, response.mutable_list_services_response());
+                break;
+            default:
+                status = Status(StatusCode::UNIMPLEMENTED, "");
+            }
+
+            if (!status.ok())
+            {
+                FillErrorResponse(status, response.mutable_error_response());
+            }
+            response.set_valid_host(request.host());
+            response.set_allocated_original_request(
+                new ServerReflectionRequest(request));
+            stream->Write(response);
+        }
+
+        return Status::OK;
+    }
+
+private:
+    grpc::Status ListService(grpc::ServerContext *context,
+                             grpc::reflection::v1alpha::ListServiceResponse *response)
+    {
+        return grpc::Status::OK;
+    }
+
+    grpc::Status GetFileByName(grpc::ServerContext *, const std::string &file_name,
+                               grpc::reflection::v1alpha::ServerReflectionResponse *response)
+    {
+        if (descriptor_pool_ == nullptr)
+        {
+            return grpc::Status::CANCELLED;
+        }
+
+        const protobuf::FileDescriptor *file_desc =
+            descriptor_pool_->FindFileByName(file_name);
+        if (file_desc == nullptr)
+        {
+            return grpc::Status(grpc::StatusCode::NOT_FOUND, "File not found.");
+        }
+        std::unordered_set<std::string> seen_files;
+        FillFileDescriptorResponse(file_desc, response, &seen_files);
+        return grpc::Status::OK;
+    }
+
+    grpc::Status GetFileContainingSymbol(
+        grpc::ServerContext *, const std::string &symbol,
+        grpc::reflection::v1alpha::ServerReflectionResponse *response)
+    {
+        if (descriptor_pool_ == nullptr)
+        {
+            return grpc::Status::CANCELLED;
+        }
+
+        const protobuf::FileDescriptor *file_desc =
+            descriptor_pool_->FindFileContainingSymbol(symbol);
+        if (file_desc == nullptr)
+        {
+            return grpc::Status(grpc::StatusCode::NOT_FOUND, "Symbol not found.");
+        }
+        std::unordered_set<std::string> seen_files;
+        FillFileDescriptorResponse(file_desc, response, &seen_files);
+        return grpc::Status::OK;
+    }
+
+    grpc::Status GetFileContainingExtension(
+        grpc::ServerContext *,
+        const grpc::reflection::v1alpha::ExtensionRequest *request,
+        grpc::reflection::v1alpha::ServerReflectionResponse *response)
+    {
+        if (descriptor_pool_ == nullptr)
+        {
+            return grpc::Status::CANCELLED;
+        }
+
+        const protobuf::Descriptor *desc =
+            descriptor_pool_->FindMessageTypeByName(request->containing_type());
+        if (desc == nullptr)
+        {
+            return grpc::Status(StatusCode::NOT_FOUND, "Type not found.");
+        }
+
+        const protobuf::FieldDescriptor *field_desc =
+            descriptor_pool_->FindExtensionByNumber(desc,
+                                                    request->extension_number());
+        if (field_desc == nullptr)
+        {
+            return grpc::Status(grpc::StatusCode::NOT_FOUND, "Extension not found.");
+        }
+        std::unordered_set<std::string> seen_files;
+        FillFileDescriptorResponse(field_desc->file(), response, &seen_files);
+        return grpc::Status::OK;
+    }
+
+    grpc::Status GetAllExtensionNumbers(
+        grpc::ServerContext *, const std::string &type,
+        grpc::reflection::v1alpha::ExtensionNumberResponse *response)
+    {
+        if (descriptor_pool_ == nullptr)
+        {
+            return grpc::Status::CANCELLED;
+        }
+
+        const protobuf::Descriptor *desc =
+            descriptor_pool_->FindMessageTypeByName(type);
+        if (desc == nullptr)
+        {
+            return grpc::Status(grpc::StatusCode::NOT_FOUND, "Type not found.");
+        }
+
+        std::vector<const protobuf::FieldDescriptor *> extensions;
+        descriptor_pool_->FindAllExtensions(desc, &extensions);
+        for (const auto &value : extensions)
+        {
+            response->add_extension_number(value->number());
+        }
+        response->set_base_type_name(type);
+        return grpc::Status::OK;
+    }
+
+    void FillFileDescriptorResponse(
+        const protobuf::FileDescriptor *file_desc,
+        grpc::reflection::v1alpha::ServerReflectionResponse *response,
+        std::unordered_set<std::string> *seen_files)
+    {
+        if (seen_files->find(file_desc->name()) != seen_files->end())
+        {
+            return;
+        }
+        seen_files->insert(file_desc->name());
+
+        protobuf::FileDescriptorProto file_desc_proto;
+        std::string data;
+        file_desc->CopyTo(&file_desc_proto);
+        file_desc_proto.SerializeToString(&data);
+        response->mutable_file_descriptor_response()->add_file_descriptor_proto(data);
+
+        for (int i = 0; i < file_desc->dependency_count(); ++i)
+        {
+            FillFileDescriptorResponse(file_desc->dependency(i), response, seen_files);
+        }
+    }
+
+    void FillErrorResponse(const Status &status,
+                           grpc::reflection::v1alpha::ErrorResponse *error_response)
+    {
+        error_response->set_error_code(status.error_code());
+        error_response->set_error_message(status.error_message());
+    }
+
+    const protobuf::DescriptorPool *descriptor_pool_;
+    const std::vector<string> *services_;
+};
--- src/ros2_grpc_bridge.cpp
+++ src/ros2_grpc_bridge.cpp
@@ -6,192 +6,109 @@
 #include <rosidl_typesupport_introspection_cpp/message_introspection.hpp>
 #include <rosidl_typesupport_protobuf/message_type_support.hpp>
 
+#include <grpc/grpc.h>
+
+#include <grpc++/grpc++.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/generic/async_generic_service.h>
+
+#include <grpc++/impl/codegen/config_protobuf.h>
+
 #include "ros/cppmsg_constructor.hpp"
-class DeserializedGenericSubscription : public rclcpp::SubscriptionBase
+#include "ros/dynamic_subscription.hpp"
+
+#include "grpc/ros_reflection.hpp"
+class RosSubscribeReactor : public grpc::ServerGenericBidiReactor
 {
 public:
-  RCLCPP_SMART_PTR_DEFINITIONS(DeserializedGenericSubscription)
-
-  template <typename AllocatorT = std::allocator<void>>
-  DeserializedGenericSubscription(
-      rclcpp::node_interfaces::NodeBaseInterface *node_base,
-      const std::string &topic_name,
-      const std::string &topic_type,
-      const rclcpp::QoS &qos,
-      std::function<void(std::shared_ptr<void>)> callback,
-      const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> &options)
-      : SubscriptionBase(
-            node_base,
-            *rclcpp::get_typesupport_handle(
-                topic_type, "rosidl_typesupport_cpp",
-                *rclcpp::get_typesupport_library(topic_type, "rosidl_typesupport_cpp")),
-            topic_name,
-            options.template to_rcl_subscription_options<rclcpp::SerializedMessage>(qos),
-            false),
-        deserialized_callback_(callback)
+  explicit RosSubscribeReactor(std::string topic)
   {
-    if (options.event_callbacks.deadline_callback)
-    {
-      this->add_event_handler(
-          options.event_callbacks.deadline_callback,
-          RCL_SUBSCRIPTION_REQUESTED_DEADLINE_MISSED);
-    }
-    if (options.event_callbacks.liveliness_callback)
-    {
-      this->add_event_handler(
-          options.event_callbacks.liveliness_callback,
-          RCL_SUBSCRIPTION_LIVELINESS_CHANGED);
-    }
-    if (options.event_callbacks.incompatible_qos_callback)
-    {
-      this->add_event_handler(
-          options.event_callbacks.incompatible_qos_callback,
-          RCL_SUBSCRIPTION_REQUESTED_INCOMPATIBLE_QOS);
-    }
-    else if (options.use_default_callbacks)
-    {
-      // Register default callback when not specified
-      try
-      {
-        this->add_event_handler(
-            [this](rclcpp::QOSRequestedIncompatibleQoSInfo &info)
-            {
-              this->default_incompatible_qos_callback(info);
-            },
-            RCL_SUBSCRIPTION_REQUESTED_INCOMPATIBLE_QOS);
-      }
-      catch (rclcpp::UnsupportedEventTypeException & /*exc*/)
-      {
-        // pass
-      }
-    }
-    if (options.event_callbacks.message_lost_callback)
-    {
-      this->add_event_handler(
-          options.event_callbacks.message_lost_callback,
-          RCL_SUBSCRIPTION_MESSAGE_LOST);
-    }
+    auto topic_type = "sensor_msgs/msg/LaserScan";
 
-    using rosidl_typesupport_introspection_cpp::MessageMembers;
+    auto proto_identifier = "rosidl_typesupport_protobuf_cpp";
+    auto proto_ts_lib = rclcpp::get_typesupport_library(
+        topic_type, proto_identifier);
+    auto proto_ts_handle = rclcpp::get_typesupport_handle(topic_type, proto_identifier, *proto_ts_lib);
+    typesupport_pb_ = static_cast<const rosidl_typesupport_protobuf::message_type_support_t *>(
+        proto_ts_handle->data);
 
-    auto typesupport_identifier = "rosidl_typesupport_introspection_cpp";
-    auto intro_lib = rclcpp::get_typesupport_library(topic_type, typesupport_identifier);
-    auto typesupport = rclcpp::get_typesupport_handle(topic_type, typesupport_identifier, *intro_lib);
+    sensor_msgs::msg::LaserScan msg;
+    std::string pbout;
+    typesupport_pb_->serialize(&msg, pbout);
 
-    members_ = static_cast<const MessageMembers *>(typesupport->data);
+    slice = grpc::Slice(pbout);
+    buf = grpc::ByteBuffer(&slice, 1);
+
+    StartWrite(&buf);
   }
 
-  std::shared_ptr<rclcpp::SerializedMessage> create_serialized_message() override
-  {
-    return std::make_shared<rclcpp::SerializedMessage>(0);
-  }
-
-  std::shared_ptr<void> create_message() override
-  {
-    using rosidl_runtime_cpp::MessageInitialization;
-
-    auto buffer = calloc(1, members_->size_of_);
-    members_->init_function(buffer, MessageInitialization::ZERO);
-
-    auto deleter = [&](void *buffer)
-    {
-      members_->fini_function(buffer);
-      free(buffer);
-    };
-    return std::shared_ptr<void>(buffer, deleter);
-  }
-
-  bool is_serialized() const
-  {
-    std::cout << "is_serialized" << std::endl;
-    return false;
-  }
-
-  bool can_loan_messages()
-  {
-    return false;
-  }
-
-  void handle_message(
-      std::shared_ptr<void> &message, const rclcpp::MessageInfo &message_info) override
-  {
-    (void)message_info;
-    deserialized_callback_(message);
-  }
-
-  void handle_loaned_message(void *, const rclcpp::MessageInfo &) override
-  {
-    throw rclcpp::exceptions::UnimplementedError(
-        "handle_loaned_message is not implemented for GenericSubscription");
-  }
-
-  void return_message(std::shared_ptr<void> &message) override
-  {
-    message.reset();
-  }
-
-  void return_serialized_message(
-      std::shared_ptr<rclcpp::SerializedMessage> &message) override
-  {
-    message.reset();
-  }
+  void OnDone() override { delete this; }
 
 private:
-  std::function<void(std::shared_ptr<void>)> deserialized_callback_;
-  const rosidl_typesupport_introspection_cpp::MessageMembers *members_;
+  std::shared_ptr<DynamicSubscription> sub_;
+  const rosidl_typesupport_protobuf::message_type_support_t *typesupport_pb_;
+  grpc::Slice slice;
+  grpc::ByteBuffer buf;
 };
 
-template <typename AllocatorT = std::allocator<void>>
-std::shared_ptr<DeserializedGenericSubscription> create_deserialized_generic_subscription(
-    rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr topics_interface,
-    const std::string &topic_name,
-    const std::string &topic_type,
-    const rclcpp::QoS &qos,
-    std::function<void(std::shared_ptr<void>)> callback,
-    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> &options = (rclcpp::SubscriptionOptionsWithAllocator<AllocatorT>()))
+class UnknownReactor : public grpc::ServerGenericBidiReactor
 {
-  auto subscription = std::make_shared<DeserializedGenericSubscription>(
-      topics_interface->get_node_base_interface(),
-      topic_name,
-      topic_type,
-      qos,
-      callback,
-      options);
+public:
+  explicit UnknownReactor() { this->Finish(grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "")); }
+  void OnDone() override { delete this; }
+};
 
-  topics_interface->add_subscription(subscription, options.callback_group);
+class RosService : public grpc::CallbackGenericService
+{
+public:
+  grpc::ServerGenericBidiReactor *CreateReactor(
+      grpc::GenericCallbackServerContext *ctx) override
+  {
+    if (ctx->method() == "lrf_head_scan/Subscribe")
+    {
+      return new RosSubscribeReactor("lrf_head_scan");
+    }
+    return new UnknownReactor;
+  }
+};
 
-  return subscription;
+void register_service()
+{
 }
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
 
-  auto node = std::make_shared<rclcpp::Node>("grpc_bridge");
-  auto topic_type = "sensor_msgs/msg/LaserScan";
+  grpc::ServerBuilder builder;
+  RosService service;
+  ProtoServerReflection reflection;
+  builder.AddListeningPort("0.0.0.0:8080", grpc::InsecureServerCredentials());
+  builder.RegisterService(&reflection);
+  builder.RegisterCallbackGenericService(&service);
 
-  auto proto_identifier = "rosidl_typesupport_protobuf_cpp";
-  auto proto_ts_lib = rclcpp::get_typesupport_library(
-      topic_type, proto_identifier);
-  auto proto_ts_handle = rclcpp::get_typesupport_handle(topic_type, proto_identifier, *proto_ts_lib);
-  auto proto_ts_support = static_cast<const rosidl_typesupport_protobuf::message_type_support_t *>(
-      proto_ts_handle->data);
+  auto server = builder.BuildAndStart();
 
-  auto sub = create_deserialized_generic_subscription(
-      node->get_node_topics_interface(), "/lrf_head_scan",
-      topic_type, rclcpp::QoS(10),
-      [&](std::shared_ptr<void> message)
-      {
-        (void)message;
-        auto test = std::static_pointer_cast<sensor_msgs::msg::LaserScan>(message);
-        std::string pbout;
-        proto_ts_support->serialize(message.get(), pbout);
-        std::cout << pbout << std::endl;
-        // RMW deserialize -> done
-        // Protobuf serialize
-      });
+  server->Wait();
+  // rclcpp::init(argc, argv);
 
-  rclcpp::spin(node);
-  rclcpp::shutdown();
+  // auto node = std::make_shared<rclcpp::Node>("grpc_bridge");
+
+  // auto sub = create_dynamic_subscription(
+  //     node->get_node_topics_interface(), "/lrf_head_scan",
+  //     topic_type, rclcpp::QoS(10),
+  //     [&](std::shared_ptr<void> message)
+  //     {
+  //       (void)message;
+  //       auto test = std::static_pointer_cast<sensor_msgs::msg::LaserScan>(message);
+  //       std::string pbout;
+  //       proto_ts_support->serialize(message.get(), pbout);
+  //       std::cout << pbout << std::endl;
+  //       // RMW deserialize -> done
+  //       // Protobuf serialize
+  //     });
+
+  // rclcpp::spin(node);
+  // rclcpp::shutdown();
   return 0;
 }
